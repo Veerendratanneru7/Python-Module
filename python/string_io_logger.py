5e63924b9297e50e82d4afc6cb7ff321d345579b
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-04 09:53:15 +0530

    Update string_io_logger.py

--- python/string_io_logger.py
+++ python/string_io_logger.py
@@ -29,22 +29,22 @@ def get_string_io_logger(log_stringio_obj, logger_name):
 
 #create string i/o object as string buffer
 log_stringio_obj = io.StringIO()
-
 log_handler = logging.StreamHandler(log_stringio_obj)
+logger = get_string_io_logger(log_stringio_obj, logger_name='my_s3_logger')
 timestamp = datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-	s3_buck = "extensionlogs"
-	s3_log_path = f"s3://{s3_buck}/python-log/{timestamp}/"
+s3_buck = "extensionlogs"
+s3_log_path = f"s3://{s3_buck}/python-log/{timestamp}/"
 
-	try:
-		#do any task
-		logger.info("Running my_function")
-		logger.info("two")
-		
-	except Exception as e:
-		exception_message = "message: {0}\nline no:{1}\n".format(str(e),sys.exc_info()[2].tb_lineno)
-		logger.error(exception_message)
-		
-	finally:
-		#persist logs in s3
-		s3_store_response = put_content_to_s3(s3_path=s3_log_path+'logs.txt', content=log_stringio_obj.getvalue())
-		assert s3_store_response['success'], "Error Putting logs to S3:\n{0}".format(s3_store_response['data'])
+try:
+	#do any task
+	logger.info("Running my_function")
+	logger.info("two")
+	
+except Exception as e:
+	exception_message = "message: {0}\nline no:{1}\n".format(str(e),sys.exc_info()[2].tb_lineno)
+	logger.error(exception_message)
+	
+finally:
+	#persist logs in s3
+	s3_store_response = put_content_to_s3(s3_path=s3_log_path+'logs.txt', content=log_stringio_obj.getvalue())
+	assert s3_store_response['success'], "Error Putting logs to S3:\n{0}".format(s3_store_response['data'])
