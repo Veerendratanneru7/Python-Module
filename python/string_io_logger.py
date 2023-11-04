25f15020d681246e72f60f662207d71a4dd48ef2
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-04 17:26:57 +0530

    Update string_io_logger.py

--- python/string_io_logger.py
+++ python/string_io_logger.py
@@ -1,7 +1,6 @@
 import io
-import os
-import sys
 import logging
+import sys
 import time
 from datetime import datetime
 
@@ -9,33 +8,60 @@ from put_content_to_s3 import put_content_to_s3
 
 
 def get_string_io_logger(log_stringio_obj, logger_name):
-    # create logger
+    # Create logger
     logger = logging.getLogger(logger_name)
     formatter = logging.Formatter(
         "%(asctime)s %(levelname)s \t[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
     )
     logger.setLevel(logging.DEBUG)
 
-    # add normal steam handler to display logs on screen
-    io_log_handler = logging.StreamHandler()
-    io_log_handler.setFormatter(formatter)
-    logger.addHandler(io_log_handler)
+    # Add a stream handler to display logs on the console
+    console_log_handler = logging.StreamHandler()
+    console_log_handler.setFormatter(formatter)
+    logger.addHandler(console_log_handler)
 
-    # create stream handler and initialise it with string io buffer
+    # Create a stream handler and initialize it with the log_stringio_obj buffer
     string_io_log_handler = logging.StreamHandler(log_stringio_obj)
     string_io_log_handler.setFormatter(formatter)
 
-    # add stream handler to logger
+    # Add the string_io_log_handler to the logger
     logger.addHandler(string_io_log_handler)
 
     return logger
 
 
-def get_logs(log_stringio_obj):
+def store_logs_in_s3(log_stringio_obj):
     timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-    s3_buck = "extensionlogs"
-    s3_log_path = f"s3://{s3_buck}/python-lambda/{timestamp}/"
-    s3_store_response = put_content_to_s3(
-        s3_path=s3_log_path + "logs.txt", content=log_stringio_obj.getvalue()
-    )
-    return log_stringio_obj.getvalue()
+    s3_bucket = "extensionlogs"
+    s3_log_path = f"s3://{s3_bucket}/python-log/{timestamp}/"
+    
+    # Store log content in S3
+    s3_store_response = put_content_to_s3(s3_path=s3_log_path + "logs.txt", content=log_stringio_obj.getvalue())
+    
+    return s3_store_response
+
+
+def main():
+    # Create a StringIO object as a string buffer
+    log_stringio_obj = io.StringIO()
+
+    # Configure the logger
+    logger = get_string_io_logger(log_stringio_obj, logger_name="my_s3_logger")
+
+    try:
+        # Perform any tasks and log messages
+        logger.info("Running my_function")
+        logger.info("two")
+
+    except Exception as e:
+        exception_message = f"message: {str(e)}\nline no: {sys.exc_info()[2].tb_lineno}\n"
+        logger.error(exception_message)
+
+    finally:
+        # Persist logs in S3
+        s3_store_response = store_logs_in_s3(log_stringio_obj)
+        assert s3_store_response["success"], f"Error Putting logs to S3:\n{s3_store_response['data']}"
+
+
+if __name__ == "__main__":
+    main()
