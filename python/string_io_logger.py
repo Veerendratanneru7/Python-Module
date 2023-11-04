070f42006d6f332f8035319e0bed86be3352f49a
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-04 16:56:50 +0530

    Update string_io_logger.py

--- python/string_io_logger.py
+++ python/string_io_logger.py
@@ -39,32 +39,3 @@ def get_logs(log_stringio_obj):
         s3_path=s3_log_path + "logs.txt", content=log_stringio_obj.getvalue()
     )
     return log_stringio_obj.getvalue()
-    
-
-# create string i/o object as string buffer
-log_stringio_obj = io.StringIO()
-log_handler = logging.StreamHandler(log_stringio_obj)
-logger = get_string_io_logger(log_stringio_obj, logger_name="my_s3_logger")
-timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-s3_buck = "extensionlogs"
-s3_log_path = f"s3://{s3_buck}/python-log/{timestamp}/"
-
-try:
-    # do any task
-    logger.info("Running my_function")
-    logger.info("two")
-
-except Exception as e:
-    exception_message = "message: {0}\nline no:{1}\n".format(
-        str(e), sys.exc_info()[2].tb_lineno
-    )
-    logger.error(exception_message)
-
-finally:
-    # persist logs in s3
-    s3_store_response = put_content_to_s3(
-        s3_path=s3_log_path + "logs.txt", content=log_stringio_obj.getvalue()
-    )
-    assert s3_store_response["success"], "Error Putting logs to S3:\n{0}".format(
-        s3_store_response["data"]
-    )
