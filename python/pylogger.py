ddbf0662f5d05fe5596417d93f3d86d474f63f9e
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-04 22:53:30 +0530

    Update pylogger.py

--- python/pylogger.py
+++ python/pylogger.py
@@ -15,7 +15,8 @@ class S3LogHandler(logging.Handler):
 
     def emit(self, record):
         log_entry = self.format(record)
-        s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/logs.txt"
+        timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+        s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/{timestamp}/logs.txt"
         put_content_to_s3(s3_log_path, log_entry)
 
 def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
@@ -41,4 +42,5 @@ def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
     return logger
 
 log_stringio_obj = io.StringIO()
-logger = get_string_io_logger(log_stringio_obj, logger_name="my_s3_logger", s3_bucket="extensionlogs", s3_prefix="python-lambda")
+logger = get_string_io_logger(log_stringio_obj, "my_s3_logger", "extensionlogs", "python-lambda")
+timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
