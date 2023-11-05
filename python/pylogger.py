347ac447991965ca44026a85d23a1516340f180e
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-05 09:00:31 +0530

    Update pylogger.py

--- python/pylogger.py
+++ python/pylogger.py
@@ -30,19 +30,8 @@ def get_string_io_logger(log_stringio_obj, logger_name):
 
     return logger
 
-
-def get_logs(log_stringio_obj):
-    timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-    s3_buck = "extensionlogs"
-    s3_log_path = f"s3://{s3_buck}/python-lambda/{timestamp}/"
-    s3_store_response = put_content_to_s3(
-        s3_path=s3_log_path + "logs.txt", content=log_stringio_obj.getvalue()
-    )
-    return log_stringio_obj.getvalue()
-    
-
 # create string i/o object as string buffer
-log_stringio_obj = io.StringIO(write_through=True)
+log_stringio_obj = io.StringIO()
 log_handler = logging.StreamHandler(log_stringio_obj)
 logger = get_string_io_logger(log_stringio_obj, logger_name="my_s3_logger")
 timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
