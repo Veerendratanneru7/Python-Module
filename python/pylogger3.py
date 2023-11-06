600c02c8273e3637cf2d8edd457c5bda7a0d9f5f
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-06 15:44:53 +0530

    Update pylogger3.py

--- python/pylogger3.py
+++ python/pylogger3.py
@@ -38,8 +38,6 @@ def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
     s3_handler = S3LogHandler(s3_bucket, s3_prefix)
     s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
-    print("content from 1")
-    print(logger)
     return logger
 
 log_stringio_obj = io.StringIO()
