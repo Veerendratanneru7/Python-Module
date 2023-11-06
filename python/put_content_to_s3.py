c553b6c5d1f83f91268641e17d0a9b12ef7dde4b
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-06 07:43:41 +0530

    Update put_content_to_s3.py

--- python/put_content_to_s3.py
+++ python/put_content_to_s3.py
@@ -9,16 +9,12 @@ def put_content_to_s3(s3_path, content, s3_client=None, s3_resource=None, region
 		'data' : ''
 	}
 	try:
-		
-		#create s3 client for given s3 path
 		bucket = s3_path.split('/')[2]
 		key = '/'.join(s3_path.split('/')[3:])
 		if not s3_client:
 			s3_client_ = boto3.client('s3', region_name)
 		else:
 			s3_client_ = s3_client
-
-		#put current content to s3
 		s3_put_response = s3_client_.put_object( Body=content, Bucket=bucket, Key=key )
 		if s3_put_response['ResponseMetadata']['HTTPStatusCode']!=200:
 			raise Exception('Unable to put data to s3: {0}'.format(s3_put_response))		
