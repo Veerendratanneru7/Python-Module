b5ab57b11973e861331283206e7bc6515d68c075
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-06 09:41:28 +0530

    Update put_content_to_s3.py

--- python/put_content_to_s3.py
+++ python/put_content_to_s3.py
@@ -25,25 +25,28 @@ def put_content_to_s3(s3_path, content, s3_client=None, s3_resource=None, region
 	finally:
 		return return_object
 		
-def get_content_from_s3(s3_path, content, s3_client=None, s3_resource=None, region_name='us-east-1'):
-	return_object = {
-		'success' : True,
-		'data' : ''
-	}
-	try:
-		bucket = s3_path.split('/')[2]
-		key = '/'.join(s3_path.split('/')[3:])
-		if not s3_client:
-			s3_client_ = boto3.client('s3', region_name)
-		else:
-			s3_client_ = s3_client
-			s3_get_response = s3_client_.get_object(Bucket=bucket, Key=key)
-			content = s3_get_response['Body'].read().decode('utf-8')
-		if s3_get_response['ResponseMetadata']['HTTPStatusCode']!=200:
-			raise Exception('Unable to put data to s3: {0}'.format(s3_put_response))		
-	except Exception as e:
-		return_object['success'] = False
-		exception_message = "message: {0}\nline no:{1}\n".format(str(e),sys.exc_info()[2].tb_lineno)
-		return_object['data'] = exception_message
-	finally:
-		return return_object
+def get_content_from_s3(s3_path, s3_client=None, s3_resource=None, region_name='us-east-1'):
+    return_object = {
+        'success': True,
+        'data': ''
+    }
+    try:
+        bucket = s3_path.split('/')[2]
+        key = '/'.join(s3_path.split('/')[3:])
+        if not s3_client:
+            s3_client_ = boto3.client('s3', region_name)
+        else:
+            s3_client_ = s3_client
+
+        s3_get_response = s3_client_.get_object(Bucket=bucket, Key=key)
+        content = s3_get_response['Body'].read().decode('utf-8')
+
+        if s3_get_response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            raise Exception('Unable to get data from S3: {0}'.format(s3_get_response))
+    except Exception as e:
+        return_object['success'] = False
+        exception_message = "message: {0}\nline no:{1}\n".format(str(e), sys.exc_info()[2].tb_lineno)
+        return_object['data'] = exception_message
+    finally:
+        return return_object
+
