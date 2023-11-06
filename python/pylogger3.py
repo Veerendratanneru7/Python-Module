08999e2de3533c960640c2df2be4984fc49ff501
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-06 15:59:29 +0530

    Update pylogger3.py

--- python/pylogger3.py
+++ python/pylogger3.py
@@ -21,16 +21,22 @@ class S3LogHandler(logging.Handler):
 
 def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
     logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s \t[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
+    )
     logger.setLevel(logging.DEBUG)
 
     io_log_handler = logging.StreamHandler()
+    io_log_handler.setFormatter(formatter)
     logger.addHandler(io_log_handler)
 
     string_io_log_handler = logging.StreamHandler(log_stringio_obj)
+    string_io_log_handler.setFormatter(formatter)
     logger.addHandler(string_io_log_handler)
 
     # Add the custom S3 handler to automatically flush logs to S3
     s3_handler = S3LogHandler(s3_bucket, s3_prefix)
+    s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
     return logger
 
