a3746ed63aa8d3ed5a6d1142cf0f732c2301ecbe
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-06 08:20:23 +0530

    Update pylogger.py

--- python/pylogger.py
+++ python/pylogger.py
@@ -7,8 +7,19 @@ from datetime import datetime
 
 from put_content_to_s3 import put_content_to_s3
 
+class S3LogHandler(logging.Handler):
+    def __init__(self, s3_bucket, s3_prefix):
+        super().__init__()
+        self.s3_bucket = s3_bucket
+        self.s3_prefix = s3_prefix
 
-def get_string_io_logger(log_stringio_obj, logger_name):
+    def emit(self, record):
+        log_entry = self.format(record)
+        timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+        s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/{timestamp}/logs.txt"
+        put_content_to_s3(s3_log_path, log_entry)
+
+def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
     logger = logging.getLogger(logger_name)
     formatter = logging.Formatter(
         "%(asctime)s %(levelname)s \t[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
@@ -22,11 +33,14 @@ def get_string_io_logger(log_stringio_obj, logger_name):
     string_io_log_handler = logging.StreamHandler(log_stringio_obj)
     string_io_log_handler.setFormatter(formatter)
     logger.addHandler(string_io_log_handler)
-    timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+
+    # Add the custom S3 handler to automatically flush logs to S3
+    s3_handler = S3LogHandler(s3_bucket, s3_prefix)
+    s3_handler.setFormatter(formatter)
+    logger.addHandler(s3_handler)
+
     return logger
-	    
+
 log_stringio_obj = io.StringIO()
-logger = get_string_io_logger(log_stringio_obj, "my_s3_logger")
+logger = get_string_io_logger(log_stringio_obj, "my_s3_logger", "extensionlogs", "python-veeru")
 timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-try:
-	print ("1")
