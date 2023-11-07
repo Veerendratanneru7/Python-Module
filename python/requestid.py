8aaeff7639c1959f20b07ad9274c329591c07e61
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-07 15:59:59 +0530

    Update requestid.py

--- python/requestid.py
+++ python/requestid.py
@@ -5,3 +5,4 @@ def capture_request_id(context):
     request_id = context.aws_request_id
     s3_client = boto3.client('s3')
     s3_client.put_object(Bucket=os.environ['S3_BUCKET'], Key=f'request-ids/{request_id}.txt', Body=request_id)
+    return request_id
