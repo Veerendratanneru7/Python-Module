e2f91bc176fcf97b16d3cdd669dfed2528629e93
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-07 22:49:13 +0530

    Update pylogger2.py

--- python/pylogger2.py
+++ python/pylogger2.py
@@ -3,23 +3,31 @@ import os
 import sys
 import logging
 import time
+import boto3
 from datetime import datetime
 from put_content_to_s3 import put_content_to_s3
-from requestid import capture_request_id 
+
+def capture_request_id(context):
+    request_id = context.aws_request_id
+    s3_client = boto3.client('s3')
+    s3_client.put_object(Bucket=os.environ['S3_BUCKET'], Key=f'request-ids/{request_id}.txt', Body=request_id)
+    print(request_id)
+    return request_id
 
 class S3LogHandler(logging.Handler):
-    def __init__(self, s3_bucket, s3_prefix):
-        super().__init__()
+    def __init__(self, s3_bucket, s3_prefix, request_id):
+        super().__init()
         self.s3_bucket = s3_bucket
         self.s3_prefix = s3_prefix
+        self.request_id = request_id
+        print(self.request_id)
 
     def emit(self, record):
         log_entry = self.format(record)
-        timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
         s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/logs.txt"
-        put_content_to_s3(s3_log_path, log_entry)
+        put_content_to_s3(s3_log_path, f"Request ID: {self.request_id}\n{log_entry}")
 
-def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
+def get_string_io_logger(log_stringio_obj, logger_name, request_id):
     logger = logging.getLogger(logger_name)
     formatter = logging.Formatter(
         "%(asctime)s %(levelname)s \t[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
@@ -34,12 +42,18 @@ def get_string_io_logger(log_stringio_obj, logger_name, s3_bucket, s3_prefix):
     string_io_log_handler.setFormatter(formatter)
     logger.addHandler(string_io_log_handler)
 
-    # Add the custom S3 handler to automatically flush logs to S3
-    s3_handler = S3LogHandler(s3_bucket, s3_prefix)
+    # Add the custom S3 handler with request_id
+    s3_bucket = os.environ.get('S3_BUCKET')
+    s3_prefix = os.environ.get('LOG_FILE_NAME')
+    s3_handler = S3LogHandler(s3_bucket, s3_prefix, request_id)
     s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
     return logger
 
+# Capture the request_id
+request_id = capture_request_id(context)
+
+# Create the logger and pass the request_id
 log_stringio_obj = io.StringIO()
-logger = get_string_io_logger(log_stringio_obj, "my_s3_logger", "extensionlogs", "python-veeru")
+logger = get_string_io_logger(log_stringio_obj, "my_s3_logger", request_id)
 timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
