4dbd28624ca0d4bc106be60f8a1fa7245cd4c5e4
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-07 14:24:49 +0530

    Create requestid.py

---
+++ python/requestid.py
@@ -0,0 +1,7 @@
+import boto3
+import os
+
+def capture_request_id(event, context):
+    request_id = context.aws_request_id
+    s3_client = boto3.client('s3')
+    s3_client.put_object(Bucket=os.environ['S3_BUCKET'], Key=f'request-ids/{request_id}.txt', Body=request_id)
