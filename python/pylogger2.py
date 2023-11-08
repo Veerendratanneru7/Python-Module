32e329537fa96caf1ba93accb5465320b499983f
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 10:50:43 +0530

    Update pylogger2.py

--- python/pylogger2.py
+++ python/pylogger2.py
@@ -48,6 +48,7 @@ def get_string_io_logger(log_stringio_obj, logger_name):
     #log_path = f"logs/os.environ['APP_CAT_ID']/os.environ['FUNCTION_NAME']/{TIMESTAMP}/os.environ['LAMBDA_NAME']/1/{unix_epoch_timestamp}.log"
     s3_prefix = f"logs/{os.environ['APP_CAT_ID']}/{os.environ['FUNCTION_NAME']}/{TIMESTAMP}/{os.environ['LAMBDA_NAME']}/1/{unix_epoch_timestamp}.log"
     print(s3_prefix)
+    test = os.environ.get('REQUEST_ID')
     s3_handler = S3LogHandler(s3_bucket, s3_prefix)
     s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
