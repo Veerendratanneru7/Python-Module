6d2991a120eab5e151185443a913a291212720fd
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 23:35:27 +0530

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,7 +1,8 @@
 import os
-
-request_id = os.environ.get('CUSTOM_REQUEST_ID')
-if request_id:
-    print(f"Request ID: {request_id}")
-else:
-    print("REQUEST_ID environment variable not found.")
+def request():
+    request_id = os.environ.get('CUSTOM_REQUEST_ID')
+    if request_id:
+        print(f"Request ID: {request_id}")
+    else:
+        print("REQUEST_ID environment variable not found.")
+    return request_id
