b8da42f0fcb5b40fe7efefe8070e5c46c59593f7
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 09:42:58 +0530

    Update pylogger2.py

--- python/pylogger2.py
+++ python/pylogger2.py
@@ -15,16 +15,15 @@ def capture_request_id(context):
     return request_id
 
 class S3LogHandler(logging.Handler):
-    def __init__(self, s3_bucket, s3_prefix, corrid):
+    def __init__(self, s3_bucket, s3_prefix):
         super().__init__()
         self.s3_bucket = s3_bucket
         self.s3_prefix = s3_prefix
-        self.corrid = corrid
 
     def emit(self, record):
         log_entry = self.format(record)
         #timestamp = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
-        s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/{self.corrid}/logs.txt"
+        s3_log_path = f"s3://{self.s3_bucket}/{self.s3_prefix}/logs.txt"
         put_content_to_s3(s3_log_path, log_entry)
 
 def get_string_io_logger(log_stringio_obj, logger_name):
@@ -45,8 +44,7 @@ def get_string_io_logger(log_stringio_obj, logger_name):
     # Add the custom S3 handler to automatically flush logs to S3
     s3_bucket = os.environ.get('S3_BUCKET')
     s3_prefix = os.environ.get('LOG_FILE_NAME')
-    corrid = os.environ['REQUEST_ID']
-    s3_handler = S3LogHandler(s3_bucket, s3_prefix, corrid)
+    s3_handler = S3LogHandler(s3_bucket, s3_prefix)
     s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
     return logger
