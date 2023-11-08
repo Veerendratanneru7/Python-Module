028b5ef026364ff9476d813b8ac346c0c5b1ddf7
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 22:50:58 +0530

    Update pylogger1.py

--- python/pylogger1.py
+++ python/pylogger1.py
@@ -24,12 +24,28 @@ class S3LogHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
         request_id = getattr(record, 'request_id', 'unknown')
+
         try:
             s3_client = boto3.client('s3')
             response = s3_client.get_object(Bucket=self.s3_bucket, Key=s3_path())
             existing_log_content = response['Body'].read().decode('utf-8')
         except s3_client.exceptions.NoSuchKey:
             existing_log_content = ""
+
+        # Construct the log entry using the custom JSON format
+        custom_log_format = {
+            "functionName": os.environ.get('LAMBDA_NAME', 'UNKNOWN'),
+            "corrId": s3_path(),
+            "serviceName": os.environ.get('SERVICE_NAME', 'UNKNOWN'),
+            "appCatId": os.environ.get('APP_CAT_ID', 'UNKNOWN'),
+            "span_id": "undefined",
+            "trace_id": "undefined",
+            "logLevel": record.levelname,
+            "detail": log_entry,
+            "timestamp": self.formatTime(record),
+        }
+
+        log_entry = json.dumps(custom_log_format)
 
         # Append the log entry to the existing content
         log_entry = existing_log_content + log_entry
@@ -38,24 +54,16 @@ class S3LogHandler(logging.Handler):
         s3_client = boto3.client('s3')
         s3_client.put_object(Bucket=self.s3_bucket, Key=s3_path(), Body=log_entry)
 
-
 def get_string_io_logger(log_stringio_obj, logger_name):
-        
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
     io_log_handler = logging.StreamHandler()
-    #io_log_handler.setFormatter(formatter)
     logger.addHandler(io_log_handler)
-
-    string_io_log_handler = logging.StreamHandler(log_stringio_obj)
-    #string_io_log_handler.setFormatter(formatter)
-    logger.addHandler(string_io_log_handler)
 
     # Add the custom S3 handler to automatically flush logs to S3
     s3_bucket = os.environ.get('S3_BUCKET')
     s3_handler = S3LogHandler(s3_bucket)
-    #s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
     return logger
 
