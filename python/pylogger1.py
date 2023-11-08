3103a443b283e2bb307d02fee71ec41e0bcc7312
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 21:13:36 +0530

    Update pylogger1.py

--- python/pylogger1.py
+++ python/pylogger1.py
@@ -5,20 +5,28 @@ import logging
 import time
 import boto3
 from datetime import datetime
-from put_content_to_s3 import put_content_to_s3
+
+TIMESTAMP = datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H")
+unix_epoch_timestamp = int(time.time())
+
+def s3_path():
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=os.environ['S3_BUCKET'], Key=f'request-ids/id.txt')
+    object_content = response['Body'].read().decode('utf-8')
+    s3_path = f"logs/{os.environ['APP_CAT_ID']}/{os.environ['FUNCTION_NAME']}/{TIMESTAMP}/{os.environ['LAMBDA_NAME']}/{object_content}/{unix_epoch_timestamp}.log"
+    return s3_path
 
 class S3LogHandler(logging.Handler):
-    def __init__(self, s3_bucket, s3_prefix):
+    def __init__(self, s3_bucket):
         super().__init__()
         self.s3_bucket = s3_bucket
-        self.s3_prefix = s3_prefix
 
     def emit(self, record):
         log_entry = self.format(record)
-        request_id = record.request_id if hasattr(record, 'request_id') else 'unknown'
+        request_id = getattr(record, 'request_id', 'unknown')
         try:
             s3_client = boto3.client('s3')
-            response = s3_client.get_object(Bucket=self.s3_bucket, Key=self.s3_prefix)
+            response = s3_client.get_object(Bucket=self.s3_bucket, Key=s3_path())
             existing_log_content = response['Body'].read().decode('utf-8')
         except s3_client.exceptions.NoSuchKey:
             existing_log_content = ""
@@ -28,10 +36,11 @@ class S3LogHandler(logging.Handler):
 
         # Upload the updated log content to S3
         s3_client = boto3.client('s3')
-        s3_client.put_object(Bucket=self.s3_bucket, Key=self.s3_prefix, Body=log_entry)
+        s3_client.put_object(Bucket=self.s3_bucket, Key=s3_path(), Body=log_entry)
 
 
 def get_string_io_logger(log_stringio_obj, logger_name):
+        
     logger = logging.getLogger(logger_name)
     formatter = logging.Formatter(
         "%(asctime)s %(levelname)s \t[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s"
@@ -48,8 +57,7 @@ def get_string_io_logger(log_stringio_obj, logger_name):
 
     # Add the custom S3 handler to automatically flush logs to S3
     s3_bucket = os.environ.get('S3_BUCKET')
-    s3_prefix = os.environ.get('LOG_FILE_NAME')
-    s3_handler = S3LogHandler(s3_bucket, s3_prefix)
+    s3_handler = S3LogHandler(s3_bucket)
     s3_handler.setFormatter(formatter)
     logger.addHandler(s3_handler)
     return logger
