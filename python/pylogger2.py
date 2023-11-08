f24ea2e28c50dc92898e8f039d79c13302368987
Veerendratanneru7 <130695503+Veerendratanneru7@users.noreply.github.com>
2023-11-08 09:37:17 +0530

    Update pylogger2.py

--- python/pylogger2.py
+++ python/pylogger2.py
@@ -11,7 +11,7 @@ def capture_request_id(context):
     request_id = context.aws_request_id
     s3_client = boto3.client('s3')
     s3_client.put_object(Bucket=os.environ['S3_BUCKET'], Key=f'request-ids/{request_id}.txt', Body=request_id)
-    print(request_id)
+    os.environ['REQUEST_ID'] = request_id
     return request_id
 
 class S3LogHandler(logging.Handler):
